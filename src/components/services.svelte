55f149e0a7652bccc0a6144c46ab37fcdfb46d4d
Nurdaulet <nurdauletaidaruli@gmail.com>
2025-12-01 23:27:30 +0500

    feat: enhance services component with GSAP scroll animations and responsive styles

--- src/components/services.svelte
+++ src/components/services.svelte
@@ -1,28 +1,105 @@
-<script>
+<script lang="ts">
   import Icon from "@iconify/svelte";
   import { SERVICES } from "../constants/services.constants";
   import { cn } from "../lib/utils";
+  import gsap from "gsap";
+  import { ScrollTrigger } from "gsap/ScrollTrigger";
+  import { onMount } from "svelte";
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  let headingDescriptionEl: HTMLDivElement;
+  let servicesButtonsEl: HTMLDivElement;
+  let learnButtonEl: HTMLButtonElement;
+  let imageDivEl: HTMLDivElement;
 
   let selectedService = $state("smart lighting");
+
+  onMount(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: headingDescriptionEl,
+        start: "top 80%",
+      },
+      defaults: { duration: 1, ease: "power2.out" },
+    });
+
+    gsap.fromTo(
+      headingDescriptionEl,
+      { autoAlpha: 0, x: -100 },
+      {
+        autoAlpha: 1,
+        x: 0,
+        duration: 1,
+        ease: "power2.out",
+        scrollTrigger: {
+          trigger: headingDescriptionEl,
+          start: "top 85%",
+        },
+      },
+    );
+    gsap.fromTo(
+      servicesButtonsEl,
+      { autoAlpha: 0, scale: 0.5 },
+      {
+        autoAlpha: 1,
+        scale: 1,
+        duration: 1,
+        scrollTrigger: {
+          trigger: servicesButtonsEl,
+          start: "top 85%",
+        },
+      },
+    );
+    gsap.fromTo(
+      learnButtonEl,
+      { autoAlpha: 0, scale: 0.5 },
+      {
+        autoAlpha: 1,
+        scale: 1,
+        duration: 1,
+        scrollTrigger: {
+          trigger: learnButtonEl,
+          start: "top 85%",
+        },
+      },
+    );
+    gsap.fromTo(
+      imageDivEl,
+      { autoAlpha: 0 },
+      {
+        autoAlpha: 1,
+        duration: 1,
+        scrollTrigger: {
+          trigger: imageDivEl,
+          start: "top 85%",
+        },
+      },
+    );
+  });
 </script>
 
 <section class="bg-foreground text-background lg:p-20">
-  <div class="mx-auto flex max-w-[1920px] gap-10">
+  <div
+    class="mx-auto my-10 flex max-w-[1920px] flex-col gap-10 px-5 py-10 lg:flex-row lg:p-0"
+  >
     <!-- Left side with buttons -->
-    <div class="mt-16 flex w-1/3 flex-col items-start justify-between">
+    <div
+      class="flex flex-col items-start justify-between gap-5 lg:mt-16 lg:w-1/3 lg:gap-0"
+    >
       <!-- Heading and description -->
-      <div class="space-y-5">
-        <h3 class="text-background inline-block text-6xl uppercase">
+      <div bind:this={headingDescriptionEl} class="space-y-5">
+        <h3 class="text-background inline-block text-4xl uppercase lg:text-6xl">
           the solutions we offer
         </h3>
-        <p class="w-[25vw] text-sm text-slate-300/60">
+        <p class="text-sm text-slate-300/60 lg:w-[25vw]">
           From installation to setup and support, we ensure your home is
           equipped with the latest technology, working smoothly and effortlessly
           - just the way you want it.
         </p>
       </div>
       <!-- Buttons for each service -->
-      <div class="flex flex-col gap-4">
+      <div bind:this={servicesButtonsEl} class="flex flex-col gap-4">
         {#each SERVICES as service, i}
           <button
             class={`cursor-pointer text-start`}
@@ -30,7 +107,7 @@
           >
             <p
               class={cn(
-                "text-2xl uppercase",
+                "text-xl uppercase lg:text-2xl",
                 selectedService === service.name && "text-secondary",
               )}
             >
@@ -42,7 +119,8 @@
       </div>
       <!-- Learn more button -->
       <button
-        class="bg-background text-foreground flex w-max scale-110 cursor-pointer items-center gap-2 rounded-4xl px-4 py-2 uppercase transition-transform hover:scale-110 lg:scale-100"
+        bind:this={learnButtonEl}
+        class="bg-background text-foreground hidden w-max scale-110 cursor-pointer items-center gap-2 rounded-4xl px-4 py-2 uppercase transition-transform hover:scale-110 lg:flex lg:scale-100"
       >
         learn more
         <Icon
@@ -56,12 +134,15 @@
 
     <!-- Image with descriptions -->
     <div
-      class="relative aspect-7/5 w-full rounded-3xl bg-[url('/services/services-photo.webp')] bg-cover bg-center"
+      bind:this={imageDivEl}
+      class="relative aspect-6/7 w-full rounded-3xl bg-[url('/services/services-photo.webp')] bg-cover bg-center lg:aspect-7/5"
     >
-      <span class="absolute top-10 right-10 text-end text-xs opacity-90"
+      <span
+        class="absolute top-10 right-10 hidden text-end text-xs opacity-90 lg:block"
         >Our services are tailored <br /> to fit your lifestyle</span
       >
-      <span class="absolute bottom-10 left-10 text-xs opacity-90"
+      <span
+        class="absolute bottom-10 left-10 hidden text-xs opacity-90 lg:block"
         >We are ready to simplify <br /> your life and enhance <br /> your living
         experience</span
       >
@@ -70,7 +151,9 @@
           class={cn(
             "absolute max-w-88 rounded-4xl border-t border-r border-white text-start backdrop-blur-md transition-all duration-300",
             service.classAbsolute,
-            selectedService === service.name ? "p-6" : "px-8 py-2 text-center",
+            selectedService === service.name
+              ? "p-6"
+              : "hidden px-8 py-2 text-center lg:block",
           )}
         >
           <h3
@@ -78,7 +161,7 @@
               "mb-2",
               selectedService === service.name
                 ? "text-2xl uppercase"
-                : "text-sm capitalize",
+                : "hidden lg:block lg:text-sm lg:capitalize",
             )}
           >
             {service.name}
@@ -110,5 +193,17 @@
         </div>
       {/each}
     </div>
+
+    <button
+      class="bg-background text-foreground flex w-max cursor-pointer items-center gap-2 place-self-center rounded-4xl px-4 py-2 uppercase transition-transform hover:scale-110 lg:hidden"
+    >
+      learn more
+      <Icon
+        icon="mdi:arrow-top-right"
+        width="44"
+        height="44"
+        class="bg-foreground text-background -my-1 -mr-3 ml-2 rounded-full p-2"
+      />
+    </button>
   </div>
 </section>
--- src/constants/services.constants.ts
+++ src/constants/services.constants.ts
@@ -1,28 +1,28 @@
 export const SERVICES = [
   {
     name: 'smart lighting',
-    classAbsolute: "top-1/2 left-1/2 translate-x-1/5 -translate-y-1/3",
+    classAbsolute: "m-4 lg:m-0 lg:top-1/2 lg:left-1/2 lg:translate-x-1/5 lg:-translate-y-1/3",
     classLine: "w-20",
     description: "Smart lighting offers a seamless way to enhance your home with both convenience and style. With a simple tap on your smartphone or a quick voice command, you can illuminate any room instantly - no more searching for switches in the dark.",
     link: "",
   },
   {
     name: 'security systems',
-    classAbsolute: "top-1/4 left-1/4 -translate-x-1/4 -translate-y-1/4",
+    classAbsolute: "m-4 lg:m-0 lg:top-1/4 lg:left-1/4 lg:-translate-x-1/4 lg:-translate-y-1/4",
     classLine: "w-20",
     description: "",
     link: "/"
   },
   {
     name: 'smart thermostats',
-    classAbsolute: "top-2/4 left-1/4 -translate-x-1/3 -translate-y-1/4",
+    classAbsolute: "m-4 lg:m-0 lg:top-2/4 lg:left-1/4 lg:-translate-x-1/3 lg:-translate-y-1/4",
     classLine: "w-20",
     description: "",
     link: "/"
   },
   {
     name: 'smart assistant',
-    classAbsolute: "top-1/4 right-1/4 translate-x-3/4 -translate-y-3/4",
+    classAbsolute: "m-4 lg:m-0 lg:top-1/4 lg:right-1/4 lg:translate-x-3/4 lg:-translate-y-3/4",
     classLine: "w-20",
     description: "",
     link: "/"
